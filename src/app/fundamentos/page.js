7014ea6a62efb06edfa21309cf94252c9b491d26
Isabelasdc <isabelasousa@msn.com>
2024-08-15 21:53:45 -0300

    criando fundamentos

--- src/app/fundamentos/page.js
+++ src/app/fundamentos/page.js
@@ -1,6 +1,18 @@
+import Cabecalho from "../components/Cabecalho";
+
 export default function Fundamentos(){
+
+  
+
+
+
     return (
+        <>
+        <Cabecalho />
         <h1>Fundamentos</h1>
+        <p>Sucessos</p>
+        <Cabecalho />
+        </>
     )
 }
 
