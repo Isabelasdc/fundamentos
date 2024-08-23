a3523cfdd83aa95d59d39d4312d42c2ecd032533
Isabelasdc <isabelasousa@msn.com>
2024-08-22 21:56:45 -0300

    Array/Objetos

---
+++ src/app/objetos/page.js
@@ -0,0 +1,30 @@
+'use client'
+
+import Pagina from "../components/Pagina"
+
+export default function Objetos() {
+
+    const carros = [
+        {marca: 'GM', modelo: 'Corsa', cor: 'Rosa', foto: ''},
+        {marca: 'GM', modelo: 'Celta', cor: 'Preto', foto: ''},
+        {marca: 'Ferrari', modelo: 'La Ferrari', cor: 'Vermelha', foto: ''},
+        {marca: 'VM', modelo: 'Fusca', cor: 'Prata', foto: ''},
+        {marca: 'GM', modelo: 'Cobalt', cor: 'Branco', foto: ''},
+    ]
+
+
+    return (
+
+        <Pagina titulo="Objetos">
+
+            {carros.map(item => (
+                <p>{item.marca}</p>
+            ))}
+
+
+        </Pagina>
+
+
+    )
+}
+
